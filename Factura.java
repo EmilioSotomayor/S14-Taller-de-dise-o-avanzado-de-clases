d6afec9fe6c097e2d0df95b6473262611322560d
EmilioSotomayor <emilio.sotomayor@udla.edu.ec>
2026-06-28 23:35:12 -0500

    Create Factura.java

---
+++ Factura.java
@@ -0,0 +1,21 @@
+public class Factura {
+    private String concepto;
+    private double valor;
+    private String nombre;
+
+    public Factura(String concepto, String nombre, double valor) {
+        this.concepto = concepto;
+        this.nombre   = nombre;
+        this.valor    = valor;
+    }
+
+    public String darConcepto() { return concepto; }
+    public double darValor()    { return valor;    }
+    public String darNombre()   { return nombre;   }
+
+    @Override
+    public String toString() {
+        return "Factura{concepto='" + concepto + "', valor=" + valor
+                + ", nombre='" + nombre + "'}";
+    }
+}
